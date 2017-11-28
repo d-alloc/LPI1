cb1c0fefbef50296ba2bfb4bcbc14691f25e89cb
Unknown <roei@pcaid.co.il>
2017-11-28 16:13:46 -0500

    HW

---
+++ HW 24-11-17/id.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Author: Roei Kessler - D-Alloc
+#Description: hold a variebles with your first name, last name,id, date of birth and country of birth and print it after 4 seconds
+#Version: 1
+#Date: 28-11-17
+name=D-
+lname=Alloc
+id=759453195
+dob=19-11-84
+cob=Israel
+sleep 4
+printf "Full Name: $name $lname\nID: $id\nDate Of Birth: $dob\nCountry Of Birth: $cob\n"
---
+++ HW 24-11-17/mkdirtree.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#Author: Roei Kessler - D-Alloc
+#Description: Create a folder tree and moves to the folder and print work folder
+#Version: 1
+#Date: 28-11-17
+mkdir -p /home/roei/kessler && cd $_
