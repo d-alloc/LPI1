1e1ea873d3a22c6722637cc8b9945a3e9e7acb20
Unknown <roei@pcaid.co.il>
2017-11-28 16:13:46 -0500

    HW

---
+++ HW 24-11-17/name.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#Author: Roei Kessler - D-Alloc
+#Description: hold a varieble with your first name and print it after 4 seconds
+#Version: 1
+#Date: 28-11-17
+name=Roei
+sleep 4
+printf "$name\n"
