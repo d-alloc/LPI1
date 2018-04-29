46985771edd5206384ccdaf92e34023be7ffc79b
Unknown <roei@pcaid.co.il>
2018-04-29 20:12:37 +0300

    Changing If To Case

--- AMPInstall.sh
+++ AMPInstall.sh
@@ -3,9 +3,9 @@
 #Date: 28-04-18
 #Description: Script that will install the following: Web Service, SQL Service and Scripting language
 ############  After installing service a webpage will be created with personal customization
-#Version: 1.0
-sqlService=$false
-webService=$false
+#Version: 1.0.1
+sqlSer=0
+webSer=0
 #Check The OS Distrebution
 #Variable 'os' saves the output of the cat which returns the OS
 #Variable 'package' hold the command that will be required later to install a package (apt-get, yum, zypper etc..)
@@ -31,6 +31,8 @@ fi
 #Installing Web Service
 #Variable 'webservice' holds user input for which service he wants to install
 
+while [ $webSer -ne 1 ]
+do
 echo "Installing Web Service"
 echo "--------------------------------------"
 echo "Please Select One Of The Option Below:"
@@ -39,23 +41,29 @@ echo "2) Ngnix"
 echo "3) Go To SQL Installation"
 read webservice
 
-if [ $webservice == 1 ]
-	then
-	$package install httpd -y
-elif [ $webservice == 2 ]
-			then
-			$package install epel-release -y
-			$package install ngnix -y
-elif [ $webservice == 3 ]
-			then
-			echo "Now We Will Install SQL Service"
-else
-			echo "Incorrect Input Please try again"
-fi
+case $webservice in
+	1)
+		$package install httpd -y
+	;;
+	2)
+		$package install epel-release -y
+                $package install ngnix -y
+	;;
+	3)
+		echo "Now We Will Install SQL Service"
+		((webSer++))
+	;;
+	*)
+		echo "Incorrect Input Please Try Again"
+	;;
+	esac
+
+done
 
 #Installing SQL Service
 #Variable 'sqlservice' holds user input	for which service he wants to install
-while [ $sqlService != ture]
+
+while [ $sqlSer -ne 1 ]
 do
 echo "Installing SQL Service"
 echo "--------------------------------------"
@@ -68,28 +76,32 @@ echo "5) mongoDB"
 echo "6) Go To Scripting Language Installation"
 read sqlservice
 
-if [ $sqlservice == 1 ]
-        then
-				wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
-        sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
-        $package update -y
-        $package install mysql-server -y
-elif [ $sqlservice == 2 ]
-        then
-				$package install mariadb -y
-elif [ $sqlservice == 3 ]
-        then
-				$package install sqlite -y
-elif [ $sqlservice == 4 ]
-        then
-				$package install postgresql -y
-elif [ $sqlservice == 5 ]
-        then
-				$package install mongodb -y
-elif [ $sqlservice == 6?ce == 6 ]
-        then
-				echo "Now We Will Install Scripting Language"
-				sqlService=$true
-else
-    		echo "Incorrect Input Please try again"
-fi
+case $sqlservice in
+	1)
+		wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+                sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+                $package update -y
+                $package install mysql-server -y
+	;;
+	2)
+		$package install mariadb -y
+	;;
+	3)
+		$package install sqlite -y
+	;;
+	4)
+		$package install postgresql -y
+	;;
+	5)
+		$package install mongodb -y
+	;;
+	6)
+		echo "Now We Will Install Scripting Language"
+                ((sqlSer++))
+	;;
+	*)
+		echo "Incorrect Input Please try again"
+	;;
+	esac
+
+done
